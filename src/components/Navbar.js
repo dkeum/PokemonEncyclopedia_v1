4b4f4aecb7189b1b0bb238cfcb32a974c0f5ef25
Daniel Keum <dkeum@sfu.ca>
2023-10-02 00:05:19 -0700

    adding navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,80 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 import Col from 'react-bootstrap/Col';
-import {LinkContainer} from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import "../css/navbar.scss";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className="">
-      
-        <Navbar.Brand href="#home">
-          <img src={require("../assets/pokemonball.png")} alt="Pokemon" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Col xs="2" className="d-flex align-items-center">
-            <div className="ms-5"> {/* Use ms-3 class for left margin */}
-              <Nav className="me-auto">
-                
-              <LinkContainer to="/PokemonEncyclopedia_v1">
-                <Nav.Link className="border border-dark rounded ms-2 bg-light">Home</Nav.Link>
-              </LinkContainer>
+    <div id="nav-wrapper">
+      <Navbar id="nav" expand="lg" className="bg-primary bg-gradient">
 
-                <LinkContainer to="/PokemonEncyclopedia_v1/pokemonencyclopedia">
-                  <Nav.Link className="border border-dark rounded ms-2 bg-light">Pokemon Encyclopedia</Nav.Link>
-                </LinkContainer>
+      <div class="nav left">
+        <span class="gradient skew pt-1">
+            <Navbar.Brand>
+              <img
+                width="85"
+                height="auto"
+                className="ms-5"
+                src={require("../assets/pokemonball.png")}
+                alt="Pokemon"
+              />  
+            </Navbar.Brand>
+          </span>
+        </div>
+
+        <div class="nav right">
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Col xs="2" className="d-flex align-items-center">
+              <div className="ms-5">
+                <Nav className="me-auto">
+                  
+                  <div class="nav-link active">
+                    <LinkContainer to="/PokemonEncyclopedia_v1">
+                        <Nav.Link className=" nav-link-span">Home</Nav.Link>
+                    </LinkContainer>
+                  </div>
+
+                  <div class="nav-link active">
+                    <LinkContainer to="/PokemonEncyclopedia_v1/pokemonencyclopedia">  
+                        <Nav.Link className="nav-link-span">Pokedex</Nav.Link>
+                    </LinkContainer>
+                  </div>
+
+                  <div class="nav-link active">
+                    <LinkContainer to="/PokemonEncyclopedia_v1/team-builder">
+                      <Nav.Link className="nav-link-span">Team Builder</Nav.Link>
+                    </LinkContainer>
+                  </div>
 
 
-                <LinkContainer to="/PokemonEncyclopedia_v1/team-builder">
-                  <Nav.Link className="border border-dark rounded ms-2 bg-light">Team Builder</Nav.Link>
-                </LinkContainer>
-
-                <NavDropdown title="MiniGames" id="basic-nav-dropdown" className="border border-dark rounded ms-2 bg-light">
-                  <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-game">
-                    <NavDropdown.Item >Guess the Pokemon</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-image-game">
-                    <NavDropdown.Item>Guess the Pokemon from Image</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-word-game">
-                    <NavDropdown.Item >Guess the Pokemon from Word</NavDropdown.Item>
-                  </LinkContainer>
-
-                </NavDropdown>
-                
-              </Nav>
-            </div>
-          </Col>
-        </Navbar.Collapse>
-    </Navbar>
+                  <div  class="nav mt-2 skew ms-2 "style={{"line-height": "5em"}} >
+                    <NavDropdown
+                      title="MiniGames"
+                      id="basic-nav-dropdown"
+                      size="lg"
+                      >
+                        <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-game" className="dropdown-item">
+                          <NavDropdown.Item >Guess the Pokemon</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-image-game" className="dropdown-item">
+                          <NavDropdown.Item>Guess the Pokemon from Image</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/PokemonEncyclopedia_v1/pokemon-word-game" className="dropdown-item">
+                          <NavDropdown.Item>Guess the Pokemon from Word</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                  </div> 
+                </Nav>
+              </div>
+            </Col>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
   );
-}
+};
 
 export default NavBar;
--- src/pages/PokemonTeamBuilder.js
+++ src/pages/PokemonTeamBuilder.js
@@ -1,11 +1,90 @@
+import Searchbox from "../components/SearchBox";
+import { Container, Col, Row } from "react-bootstrap";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const PokemonTeamBuilder = () =>{
-    return(
-        <>
-        <h1>Pokemon Team Builder</h1>
-        </>
-    );
-}
+const PokemonTeamBuilder = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonEggs, setPokemonEggs] = useState([{}, {}, {}, {}, {}, {}]);
+  const [pokemon, setPokemon] = useState(""); // Keep the pokemon state
+
+  // Get all Pokémon on mount
+  useEffect(() => {
+    const getAllPokemon = async () => {
+      try {
+        const url = `https://pokeapi.co/api/v2/pokemon/?limit=811`;
+        const res = await axios.get(url);
+        setPokemonData(res.data.results);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getAllPokemon();
+    // eslint-disable-next-line
+  }, []);
+
+  // Function to generate 6 random Pokémon and update pokemonEggs
+  const generateRandomTeam = () => {
+    const randomIndices = [];
+    while (randomIndices.length < 6) {
+      const randomIndex = Math.floor(Math.random() * pokemonData.length);
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex);
+      }
+    }
+
+    const newPokemonEggs = randomIndices.map((index) => ({
+      name: pokemonData[index].name,
+      url: pokemonData[index].url,
+    }));
+
+    setPokemonEggs(newPokemonEggs);
+  };
+
+  // Function to add a single Pokémon (you can customize this)
+  const addPokemon = () => {
+    console.log(`Adding Pokemon: ${pokemon}`);
+    setPokemon(); // cjange
+  };
+
+  return (
+    <>
+    {/* searchPokemon={searchPokemon} pokemon={pokemon} */}
+      <Searchbox /> 
+      <button onClick={addPokemon}>Add Pokemon</button>
+      <button onClick={generateRandomTeam}>Generate Random Team</button>
+      <Container>
+        <Row>
+          {pokemonEggs.map((pokemon, i) => (
+            <Col key={i}>
+              {pokemon.url ? (
+                <>
+                <img
+                  key={`pokemon-${i}`}
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.substring(
+                    34,
+                    34 + Math.abs(pokemon.url.length - 36 + 1)
+                  )}.png`}
+                  alt="pokemon"
+                />
+                    <p>{pokemon.name}</p>
+                </>
+                
+              ) : (
+                <img
+                    width="200px"
+                    height="auto"
+                  src={require("../assets/PokemonEgg.webp")}
+                  alt="pokemon-egg"
+                />
+              )}
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default PokemonTeamBuilder;
